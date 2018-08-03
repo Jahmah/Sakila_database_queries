7961a13ee936b0c142194bb1180eb30e9436a551
Jamah <jahmahlon@gmail.com>
2018-08-03 08:47:31 -0400

    Sixth Update

--- sakila_db_queries.sql
+++ sakila_db_queries.sql
@@ -4,11 +4,12 @@
 
 USE sakila;
 
--- 1a. Display the first and last names of all actors from the table `actor`. --
+-- 1a. Display the first and last names of all actors from the table actor. 
 
 SELECT first_name, 
 	   last_name
-FROM    actor;
+FROM   actor;
+
 
 -- 1b. Display the first and last name of each actor in a single column in upper case letters.
 --     Name the column Actor Name.
@@ -18,8 +19,7 @@ AS    'Actor Name'
 FROM   actor;
 
 
--- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
---      What is one query would you use to obtain this information?
+-- 2a. find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." 
 
 SELECT actor_id, 
        first_name, 
@@ -28,7 +28,7 @@ FROM   actor
 WHERE  first_name = 'Joe';
 
 
--- 2b. Find all actors whose last name contain the letters GEN:
+-- 2b. Find all actors whose last name contain the letters 'GEN':
 
 SELECT * 
 FROM  actor 
@@ -36,16 +36,16 @@ WHERE last_name LIKE '%GEN%';
 
 
 -- 2c. Find all actors whose last names contain the letters LI. 
---     This time, order the rows by last name and first name, in that order:
+--     order the rows by last name and first name, in that order:
 
 SELECT * 
 FROM  actor 
 WHERE last_name LIKE '%LI%'
-ORDER BY last_name, first_name;
+ORDER BY last_name,
+         first_name;
 
 
--- 2d. Using IN, display the country_id and country columns of the following countries: 
---     Afghanistan, Bangladesh, and China:
+-- 2d. Using IN, display the country_id and country columns of Afghanistan, Bangladesh, and China:
 
 SELECT country_id, 
        country
@@ -53,12 +53,11 @@ FROM   country
 WHERE  country IN ('Afghanistan', 'Bangladesh', 'China');
 
 
--- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description,
---     so create a column in the table `actor` named `description` and use the data type `BLOB` 
---     (Make sure to research the type `BLOB`, as the difference between it and `VARCHAR` are significant).
+-- 3a. Create a column in the table `actor` named `description` and use the data type `BLOB` 
 
 ALTER TABLE sakila.actor 
 ADD COLUMN description BLOB NOT NULL;
+
 
 
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. 
